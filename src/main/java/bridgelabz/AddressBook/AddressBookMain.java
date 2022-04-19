4758aa816adc45c12146e1b9ac5e1a86f112adc1
sameers999 <syedmahammadsameer999>
2022-04-19 06:44:33 +0530

    Added Address Book

---
+++ src/main/java/bridgelabz/AddressBook/AddressBookMain.java
@@ -0,0 +1,42 @@
+package bridgelabz.AddressBook;
+
+import java.util.Scanner;
+public class AddressBookMain {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Address Book");
+		AddressBookMain.showMainMenu();
+	}
+	
+	public static void showMainMenu() {
+		System.out.println(
+				"\n Please select from the below options to continue in :\n 1.Add new contact \n 2.Delete contact \n 3.print current contact \n 4. Exit \n 5.Reselect options");
+		Contact con = new Contact();
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in); 
+		int option = sc.nextInt();
+		switch (option) {
+		case 1:
+			con.addnewContact();
+			showMainMenu();
+			break;
+		case 2:
+			con.deleteContact();
+			showMainMenu();
+			break;
+		case 3:
+			con.printContact();
+			showMainMenu();
+			break;
+		case 4:
+			
+			System.out
+					.println("you can exit....\n Thankyou");
+			break;
+		case 5:
+			showMainMenu();
+			break;
+		}
+	}
+}
---
+++ src/main/java/bridgelabz/AddressBook/NewBook.java
@@ -0,0 +1,50 @@
+package bridgelabz.AddressBook;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class NewBook extends AddressBookMain {
+
+	List<NewBook> newbook = new ArrayList<NewBook>();
+	static String addbook;
+
+	public void newAddressBook() {
+
+		Iterator<NewBook> newBook = newbook.iterator();
+		Scanner s = new Scanner(System.in);
+		System.out.println(
+				"enter the number Addressbook to store");
+		int n = s.nextInt();
+		s.close();
+		for (int i = 0; i < n; i++) {
+			if (i <= n) {
+				System.out.println(
+						"Enter the name of first Address Book");
+				addbook = s.next();
+				showMainMenu();
+				printBook();
+			}
+		}
+	}
+	public List<NewBook> printBook() {
+		System.out.println(
+				"-----------------------Print AddressBook--------------------------------------------\n");
+
+		if (newbook.isEmpty()) {
+			System.out.println("No Records FOUND!!!");
+		} else {
+			for (@SuppressWarnings("unused") NewBook newBook : newbook) {
+				System.out.println(newbook);
+			}
+		}
+		return newbook;
+	}
+
+	@Override
+	public String toString() {
+		return "NewBook [newbook=" + newbook + ", addbook="
+				+ addbook + "]";
+	}
+}
